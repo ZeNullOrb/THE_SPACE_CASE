7943caa698d21c56dd1b398d51458ea4862e675f
ZeNullOrb <elyas1999sb@hotmail.com>
2023-02-23 02:28:11 +0300

    build the models

--- TheSpaceCase/main/admin.py
+++ TheSpaceCase/main/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from .models import Contact,Company,Certificate,Post
 # Register your models here.
+
+
+#to customize the panel
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ()
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ()
+
+
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ()
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ()
+
+
+
+admin.site.register(Contact,ContactAdmin)
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Certificate,CertificateAdmin)
+admin.site.register(Post,PostAdmin)
--- TheSpaceCase/main/models.py
+++ TheSpaceCase/main/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+
+    name = models.CharField(max_length=512)
+    email = models.EmailField()
+    subject = models.CharField(max_length=512)
+    message = models.TextField()
+
+class Company(models.Model):
+
+    name = models.CharField(max_length=512)
+    email = models.EmailField()
+    address = models.CharField(max_length=1024)
+    phone = models.CharField(max_length=1024)
+    website = models.URLField()
+    description = models.TextField()
+
+class Certificate(models.Model):
+
+    name = models.CharField(max_length=512)
+    issuing_organization = models.CharField(max_length=1024)
+    issue_date = models.DateField()
+    expiration_date = models.DateField()
+    certificate_id = models.IntegerField()
+    certificate_url = models.URLField()
+
+class Post(models.Model):
+    title = models.CharField(max_length=512)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    certificate = models.ForeignKey(Certificate, on_delete=models.CASCADE)
+    
